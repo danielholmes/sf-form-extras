2030af2db44f417225dd47a42e70e6b883e67804
Daniel Holmes <daniel@creatio.com.au>
2012-05-29 21:45:31 +1000

    Updated for latest version fo form component

--- src/DHolmes/SfFormExtras/EnumerationType.php
+++ src/DHolmes/SfFormExtras/EnumerationType.php
@@ -3,12 +3,12 @@
 namespace DHolmes\SfFormExtras;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\Options;
 
 class EnumerationType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     { 
         if ($options['multiple'])
         {
